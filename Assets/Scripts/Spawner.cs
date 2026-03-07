0e069a18f5c0237289ebcc403534ae96fe2c4cca
KronoEE <juanibernardi019@gmail.com>
2026-03-07 05:48:17 -0300

    New modify
    - Setted max enemy to spawn in spawner
    - Fixed sound bug in main menu

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,6 +6,10 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject zombiePrefab;
     [SerializeField] private Transform spawnPoint;
     [SerializeField] private float interval = 3.5f;
+    [SerializeField] private int maxEnemies = 10;
+
+    private int count = 0;
+
     void Start()
     {
         StartCoroutine(spawnEnemy(interval, zombiePrefab));
@@ -16,5 +20,11 @@ public class EnemySpawner : MonoBehaviour
         yield return new WaitForSeconds(interval);
         GameObject newEnemy = Instantiate(enemy, spawnPoint.position, spawnPoint.rotation);
         StartCoroutine(spawnEnemy(interval, enemy));
+        count++;
+
+        if (count == maxEnemies)
+        {
+            Destroy(gameObject);
+        }
     }
 }
